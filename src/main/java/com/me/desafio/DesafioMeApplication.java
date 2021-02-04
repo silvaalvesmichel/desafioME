2fa6ec97e317cddcfb08319ae81afd3337114259
Michel <silvaalvesmichel07@outlook.com>
2021-02-04 18:11:36 -0300

    first commit

---
+++ src/main/java/com/me/desafio/DesafioMeApplication.java
@@ -0,0 +1,13 @@
+package com.me.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/me/desafio/DesafioMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
